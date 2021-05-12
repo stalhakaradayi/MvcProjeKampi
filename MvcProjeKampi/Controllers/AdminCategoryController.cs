6818866ebd6563998f1ff48580e28b7e6320433d
stalhakaradayi <sukrutalhakaradayi@gmail.com>
2021-05-12 11:18:25 +0300

    without statics - 12-05-2021

--- MvcProjeKampi/Controllers/AdminCategoryController.cs
+++ MvcProjeKampi/Controllers/AdminCategoryController.cs
@@ -43,5 +43,23 @@ namespace MvcProjeKampi.Controllers
             }
             return View();
         }
+        public ActionResult DeleteCategory(int id)
+        {
+            var categoryValue = cm.GetByID(id);
+            cm.CategoryDelete(categoryValue);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult EditCategory(int id)
+        {
+            var categoryValue = cm.GetByID(id);
+            return View(categoryValue);
+        }
+        [HttpPost]
+        public ActionResult EditCategory(Category p)
+        {
+            cm.CategoryUpdate(p);
+            return RedirectToAction("Index");
+        }
     }
 }
